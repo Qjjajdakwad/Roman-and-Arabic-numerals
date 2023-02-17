40e68e5ca972084cff7bf07fd8684efae591caa2
Qjjajdakwad <59441153+Qjjajdakwad@users.noreply.github.com>
2023-02-17 16:07:11 +0500

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,10 +1,14 @@
 #module (Library)
 roman_table = [(1000, 'M'), (900, 'CM'), (500, 'D'), (400, 'CD'), (100, 'C'), (90, 'XC'), (50, 'L'), (40, 'XL'), (10, 'X'), (9, 'IX'), (5, 'V'), (4, 'IV'), (1, 'I')]
 
+
 def roman_to_int(i):
-
-
-
+    Arab = 0
+    for int, rome in roman_table:
+        while i.startswith(rome):
+            Arab += int
+            i = i[len(rome):]
+    return Arab
 
 
 def int_to_roman(i):
